9e61e4c5bcf08f1000d00ed2420fc1eb23b894b7
aziret <aziret.toktoraliev@iaau.edu.kg>
2017-04-24 18:27:24 +0600

    backend: working on adding member

--- src/main/java/kg/goent/controllers/ProjectMemberController.java
+++ src/main/java/kg/goent/controllers/ProjectMemberController.java
@@ -1,10 +1,16 @@
 package kg.goent.controllers;
 
+import kg.goent.facade.MemberRoleFacade;
+import kg.goent.facade.UserFacade;
+import kg.goent.models.MemberRole;
 import kg.goent.models.ProjectMember;
+import kg.goent.models.User;
+import kg.goent.tools.Tools;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.lang.reflect.Member;
 
 /**
  * Created by b-207 on 4/21/2017.
@@ -33,14 +39,10 @@ public class ProjectMemberController {
     }
 
     public String getUserEmail() {
-        if(projectMember.getUser() != null){
-            return projectMember.getUser().getEmail();
-        }
         return userEmail;
     }
 
     public void setUserEmail(String userEmail) {
-        projectMember.getUser().setEmail(null);
         this.userEmail = userEmail;
     }
 
@@ -50,5 +52,25 @@ public class ProjectMemberController {
 
     public void setMemberRole(String memberRole) {
         this.memberRole = memberRole;
+    }
+
+    public String addMember(){
+        User u = new UserFacade().findByEmail(userEmail);
+        if(u == null || u.getEmail() == null){
+            Tools.faceMessageWarn("User doesn't exist.","Please, look if email is correct.");
+            return "";
+        }
+        MemberRole mr = new MemberRoleFacade().findByRole(memberRole);
+        if(mr == null || mr.getMemberRole() == null || mr.getMemberRoleId() != 1 || mr.getMemberRoleId() != 0){
+            Tools.faceMessageWarn("User doesn't exist.","Please, look if email is correct.");
+            return "";
+        }
+
+        projectMember.setUser(u);
+        projectMember.setMemberRole(mr);
+        
+
+
+        return "";
     }
 }
